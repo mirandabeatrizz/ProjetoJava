9e712eb5af42f6ea0628a9be5fe589b07207bd53
Beatriz Miranda <mirandabeatrizaparecida@gmail.com>
2023-05-10 22:54:40 -0300

    projeto

---
+++ src/main/java/com/mycompany/projeto/Projeto.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto;
+
+import com.mycompany.projeto.view.Login;
+
+/**
+ *
+ * @author beatriz.miranda
+ */
+public class Projeto {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+       
+        
+    }
+}
